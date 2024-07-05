6c7cb25e2912cda705268791326208d6b218020c
PayaCodes <joshuaborland20@gmail.com>
2024-07-05 16:21:31 +0100

    Update calc.py
    Just a calculator

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
-num1 = input(int())
+num1 = input("Enter a Number: ")
 inbetween = input("Enter an operator: ")
-num2 = input(int())
+num2 = input("Enter another Number: ")
 number = 0
 
 num1 = int(num1)
@@ -15,10 +15,10 @@ elif inbetween == "*":
 elif inbetween == "/":
   number = (num1 / num2)
 elif inbetween == "**":
-  number = (num1 ** num2)
+    number = (num1 ** num2)
 elif inbetween == "//":
-  number = (num1 // num2)
+    number == (num1 // num2)
 else:
   print("Invalid calculation")
 
-print(number)
+print(f"The answer is : {number}")
