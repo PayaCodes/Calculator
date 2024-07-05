ab37ddad2f6ef8001b21b04501562a93b7e1d068
PayaCodes <joshuaborland20@gmail.com>
2024-07-05 13:37:35 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,20 @@
+num1 = input(int())
+inbetween = input("Enter an operator: ")
+num2 = input(int())
+number = 0
+
+num1 = int(num1)
+num2 = int(num2)
+
+if inbetween == "+":
+  number = (num1 + num2)
+elif inbetween == "-":
+  number = (num1 - num2)
+elif inbetween == "*":
+  number = (num1 * num2)
+elif inbetween == "/":
+  number = (num1 / num2)
+else:
+  print("Invalid calculation")
+
+print(number)
